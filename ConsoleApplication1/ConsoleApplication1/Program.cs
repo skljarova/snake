cbe6833452b17af6540fc887ba46a3e28cdcd1b3
skljarova <nnezumi@mail.ru>
2018-09-19 10:54:46 +0500

    Горизонтальня и вертикальная линии

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -17,44 +17,11 @@ namespace ConsoleApplication1
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
+            HorizontalLine Line = new HorizontalLine(5, 10, 8, '+');
+            Line.Draw();
 
-            Point p3 = new Point(7, 7, '$');
-            p3.Draw();
-
-            Point p4 = new Point(10, 9, '^');
-            p4.Draw();
-
-            Point p5 = new Point(12, 11, '@');
-            p5.Draw();
-
-            Point p6 = new Point(15, 13, '%');
-            p6.Draw();
-
-           List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
-
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-
-            foreach(int i in numList)
-            {
-            Console.WriteLine();
-            }
-
-           
-
-            numList.RemoveAt(0);
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
-            pList.Add(p3);
-            pList.Add(p4);
-            pList.Add(p5);
-            pList.Add(p6);
+            VerticalLine vline = new VerticalLine(5, 5, 10, '+');
+            vline.Draw();
 
 
             Console.ReadLine();
---
+++ ConsoleApplication1/ConsoleApplication1/VerticalLine.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+        class VerticalLine
+        {
+            List<Point> pList;
+
+            public VerticalLine(int x, int yUp, int yDown, char sym)
+            {
+                pList = new List<Point>();
+
+                for (int y = yUp; y <= yDown; y++)
+                {
+                    Point p = new Point(x, y, sym);
+                    pList.Add(p);
+                }
+            }
+
+            public void Draw()
+            {
+                foreach (Point p in pList)
+                {
+
+                    p.Draw();
+                }
+
+            }
+        }
+    }
+

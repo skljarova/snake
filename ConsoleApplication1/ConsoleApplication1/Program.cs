afb3b593f88bdf72ef449428990c7c0d1eab2c61
skljarova <nnezumi@mail.ru>
2018-09-19 10:18:27 +0500

    Класс List

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -9,7 +9,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*')
+            Point p1 = new Point(1, 3, '*');
             p1.Draw();
 
        
@@ -17,8 +17,46 @@ namespace ConsoleApplication1
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
+
+            Point p3 = new Point(7, 7, '$');
+            p3.Draw();
+
+            Point p4 = new Point(10, 9, '^');
+            p4.Draw();
+
+            Point p5 = new Point(12, 11, '@');
+            p5.Draw();
+
+            Point p6 = new Point(15, 13, '%');
+            p6.Draw();
+
+           List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+            foreach(int i in numList)
+            {
+            Console.WriteLine();
+            }
+
            
-            
+
+            numList.RemoveAt(0);
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+            pList.Add(p4);
+            pList.Add(p5);
+            pList.Add(p6);
+
+
             Console.ReadLine();
         }
 
